21016912287d7d35d90d09d238ef09a7d28d0194
AminTaiTntra <amin.tai@tntra.io>
2025-01-22 11:57:21 +0530

    Temporary Message Updated

--- src/config/config.js
+++ src/config/config.js
@@ -18,7 +18,7 @@ const envVarsSchema = Joi.object()
     JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
       .default(10)
       .description('minutes after which verify email token expires'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
+    SMTP_HOST: Joi.string().description('Message Updated: server that will send the emails'),
     SMTP_PORT: Joi.number().description('port to connect to the email server'),
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
